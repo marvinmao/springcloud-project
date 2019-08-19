c191e72ff8982d5f868f0e6b1d126b15300c6785
maofujiang <1512355855@qq.com>
2019-08-19 22:19:20 +0800

    product

--- microcloud-api/src/main/java/cn.enjoy.vo/Product.java
+++ microcloud-api/src/main/java/cn.enjoy.vo/Product.java
@@ -1,5 +1,7 @@
 package cn.enjoy.vo;
+
 import java.io.Serializable;
+
 public class Product implements Serializable {
 
     private Long productId;
@@ -9,21 +11,27 @@ public class Product implements Serializable {
     public String getProductDesc() {
         return productDesc;
     }
+
     public void setProductDesc(String productDesc) {
         this.productDesc = productDesc;
     }
+
     public String getProductName() {
         return productName;
     }
+
     public void setProductName(String productName) {
         this.productName = productName;
     }
+
     public Long getProductId() {
         return productId;
     }
+
     public void setProductId(Long productId) {
         this.productId = productId;
     }
+
     @Override
     public String toString() {
         return "Product{" +
